3e044299cefe3ec73f646f5dfc1a3c7cd6e50882
Spalishe <maksamoshkin2010@gmail.com>
2026-06-01 22:10:40 +0300

    Missplaced LICENSE Notices

--- memtester/uart.c
+++ memtester/uart.c
@@ -1,16 +1,18 @@
-/*
- * memtester version 4
+/*Copyright (C) 2026  Spalishe
  *
- * Very simple but very effective user-space memory tester.
- * Originally by Simon Kirby <sim@stormix.com> <sim@neato.org>
- * Version 2 by Charles Cazabon <charlesc-memtester@pyropus.ca>
- * Version 3 not publicly released.
- * Version 4 rewrite:
- * Copyright (C) 2004-2020 Charles Cazabon <charlesc-memtester@pyropus.ca>
- * Copyright (C) 2026 Spalishe <ynororah@gmail.com>
- * Licensed under the terms of the GNU General Public License version 2 (only).
- * See the file LICENSE for details.
- * Code was modified
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #define UART_BASE 0x10000000
